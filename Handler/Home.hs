bb81d0ea3ae5a3c2a4136fcff3fd577879546813
Ori Pekelman <ori@pekelman.com>
2012-12-17 16:34:17 +0100

    trying to see what hamlet does

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -12,28 +12,10 @@ import Data.Time (UTCTime)
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler RepHtml
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
+    -- Get the list of articles inside the database.
+    posts <- runDB $ selectList [] [Desc PostTitle]
+    -- We'll need the two "objects": articleWidget and enctype
+    -- to construct the form (see templates/posts.hamlet).
+    (postWidget, enctype) <- generateFormPost entryForm
     defaultLayout $ do
-        aDomId <- lift newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-postHomeR :: Handler RepHtml
-postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
-
-    defaultLayout $ do
-        aDomId <- lift newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderDivs $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
+        $(widgetFile "posts")
