cd88a28200f427ac0d3f496484ce981dabb7df24
Ori Pekelman <ori@pekelman.com>
2012-12-17 15:40:14 +0100

    add post handler and post template

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -22,9 +22,27 @@ entryForm = renderDivs $ Article
 getBlogR :: Handler RepHtml
 getBlogR = do
     -- Get the list of articles inside the database.
-    articles <- runDB $ selectList [] [Desc PostTitle]
+    posts <- runDB $ selectList [] [Desc PostTitle]
     -- We'll need the two "objects": articleWidget and enctype
-    -- to construct the form (see templates/articles.hamlet).
-    (articleWidget, enctype) <- generateFormPost entryForm
+    -- to construct the form (see templates/posts.hamlet).
+    (postWidget, enctype) <- generateFormPost entryForm
     defaultLayout $ do
         $(widgetFile "posts")
+-- we continue Handler/Blog.hs for posting
+postBlogR :: Handler RepHtml
+postBlogR = do
+    ((res,postWidget),enctype) <- runFormPost entryForm
+    case res of 
+         FormSuccess post -> do 
+            postId <- runDB $ insert post
+            setMessage $ toHtml $ (postTitle post) <> " created"
+            redirect $ PostR postId 
+         _ -> defaultLayout $ do
+                setTitle "Please correct your entry form"
+                $(widgetFile "postAddError")         
+getPostR :: PostId -> Handler RepHtml
+getPostR postId = do
+    post <- runDB $ get404 postId
+    defaultLayout $ do
+        setTitle $ toHtml $ postTitle post
+        $(widgetFile "post")
